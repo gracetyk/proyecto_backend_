29dc3fb4e41b1bfc05e1afbca55424dda4fe49a2
acrisanto <alanvcrisanto@gmail.com>
2022-03-06 17:01:11 -0500

    chore: update en aws imagenes

--- src/services/archivos.service.js
+++ src/services/archivos.service.js
@@ -1,9 +1,15 @@
 import { s3 } from "../s3.js";
+import {prisma} from '../prisma.js';
 
 export class ArchivosService {
   // data = { productoId: 1, contentType: 'image/png', ext:'png', filename: 'mayonesa.png' }
   static async crearArchivo(data) {
-    
+
+    // const localEncontrado = await prisma.local.findUnique({
+    //   where: {id: data.productoId},
+    //   select: {fotos: true},
+    //   rejectOnNotFound: false,
+    // })
     const path = `archivos/productos/${data.productoId}`;
 
     const url = s3.getSignedUrl("putObject", {
